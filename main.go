df32e15747b0833fd1f32ee6a19932a637426874
Jose Luis Vázquez González <josvazg@gmail.com>
2012-04-23 18:15:28 +0200

    Using sort.IntsAreSorted

--- main.go
+++ main.go
@@ -6,20 +6,6 @@ import (
 	"time"
 )
 
-func sorted(list []int) bool {
-	if len(list) <= 1 {
-		return true
-	}
-	last := list[0]
-	for i := 1; i < len(list); i++ {
-		if list[i] < last {
-			return false
-		}
-		last = list[i]
-	}
-	return true
-}
-
 var tests = [][]int{
 	{9, 6, 5, 3, 1, 8, 7, 2, 4},
 	{9, 6, 5, 3, 1, 8, 7, 2, 4, 0},
@@ -40,27 +26,27 @@ func main() {
 	for _, u := range tests {
 		list := make([]int, len(u))
 		fmt.Println("Unsorted:", u)
-		for _, sort := range sorts {
+		for _, asort := range sorts {
 			copy(list, u)
-			sort.fn(list)
-			fmt.Println(sort.name, " sorted list:", list)
-			if !sorted(list) {
+			asort.fn(list)
+			fmt.Println(asort.name, " sorted list:", list)
+			if !sort.IntsAreSorted(list) {
 				fmt.Println("FAILED!")
 				break
 			}
 		}
 	}
 	times := 500000
-	for _, sort := range sorts {
+	for _, asort := range sorts {
 		t := time.Now()
 		for i := 0; i < times; i++ {
 			for _, u := range tests {
 				list := make([]int, len(u))
 				copy(list, u)
-				sort.fn(list)
+				asort.fn(list)
 			}
 		}
-		fmt.Println(sort.name, " sorted", times, "times in", time.Since(t))
+		fmt.Println(asort.name, " sorted", times, "times in", time.Since(t))
 	}
 }
 
