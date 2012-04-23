7b6db537e83702b5fd55f098546ed685c2d670d2
Jose Luis Vázquez González <josvazg@gmail.com>
2012-04-23 18:10:08 +0200

    various mergesort compared with go standard sort

--- main.go
+++ main.go
@@ -2,26 +2,65 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"time"
 )
 
 func sorted(list []int) bool {
-	if len(list)<=1 {
+	if len(list) <= 1 {
 		return true
 	}
-	last:=list[0]
-	for i:=1;i<len(list);i++ {
-		if list[i]<last {
+	last := list[0]
+	for i := 1; i < len(list); i++ {
+		if list[i] < last {
 			return false
 		}
-		last=list[i]
+		last = list[i]
 	}
 	return true
 }
 
-func main() {
-	u:=[]int{9,6,5,3,1,8,7,2,4}
-	fmt.Println("Unsorted:",u)
-	s:=mergesort(u)
-	fmt.Println("Sorted list:",s)
-	fmt.Println("Sorted?",sorted(s))
+var tests = [][]int{
+	{9, 6, 5, 3, 1, 8, 7, 2, 4},
+	{9, 6, 5, 3, 1, 8, 7, 2, 4, 0},
+	{9, 8, 7, 6, 5, 4, 3, 2, 1},
+	{9, 9, 9, 2, 3, 4, 2, 4, 5, 7, 6, 8, 3, 5},
 }
+
+var sorts = []struct {
+	name string
+	fn   func([]int)
+}{
+	{"mergesort", mergesort},
+	{"swap mergesort", smergesort},
+	{"sort.Ints", sort.Ints},
+}
+
+func main() {
+	for _, u := range tests {
+		list := make([]int, len(u))
+		fmt.Println("Unsorted:", u)
+		for _, sort := range sorts {
+			copy(list, u)
+			sort.fn(list)
+			fmt.Println(sort.name, " sorted list:", list)
+			if !sorted(list) {
+				fmt.Println("FAILED!")
+				break
+			}
+		}
+	}
+	times := 500000
+	for _, sort := range sorts {
+		t := time.Now()
+		for i := 0; i < times; i++ {
+			for _, u := range tests {
+				list := make([]int, len(u))
+				copy(list, u)
+				sort.fn(list)
+			}
+		}
+		fmt.Println(sort.name, " sorted", times, "times in", time.Since(t))
+	}
+}
+
--- mergesort.go
+++ mergesort.go
@@ -1,72 +1,78 @@
 package main
 
-import (
-	"fmt"
-)
-
-func mergesort(list []int) []int {
-	if(len(list)==1) {
-		return list
+func mergesort(list []int) {
+	if len(list) == 1 {
+		return
 	}
-	fmt.Println("unordered list=",list)
-	for size:=1;size<len(list);size=2*size {
-		for i:=0;i<len(list)-size;i+=2*size {
-			limit:=i+(2*size)
-			if limit>len(list) {
-				limit=len(list)
+	for size := 1; size < len(list); size = 2 * size {
+		for i := 0; i < len(list)-size; i += 2 * size {
+			limit := i + (2 * size)
+			if limit > len(list) {
+				limit = len(list)
 			}
-			merge(list[i:limit],size)
+			merge(list[i:limit], size)
 		}
 	}
-	return list
 }
 
 func merge(list []int, size int) {
-	if size==1 {
-		if list[1]<list[0] {
-			list[0],list[1]=list[1],list[0]
+	if size == 1 {
+		if list[1] < list[0] {
+			list[0], list[1] = list[1], list[0]
 		}
 		return
 	}
-	tmp:=[]int{}
-	a:=list[0:size]
-	b:=list[size:]
-	for ;len(a)>0 || len(b)>0; {
-		if len(a)>0 && len(b)>0 {
-			if a[0]<b[0] {
-				tmp=append(tmp,a[0])
-				a=a[1:]
+	tmp := []int{}
+	a := list[0:size]
+	b := list[size:]
+	for len(a) > 0 || len(b) > 0 {
+		if len(a) > 0 && len(b) > 0 {
+			if a[0] < b[0] {
+				tmp = append(tmp, a[0])
+				a = a[1:]
 			} else {
-				tmp=append(tmp,b[0])
-				b=b[1:]
+				tmp = append(tmp, b[0])
+				b = b[1:]
 			}
-		} else if len(a)>0 {
-			tmp=append(tmp,a...)
-			a=[]int{}
-		} else if len(b)>0 {
-			tmp=append(tmp,b...)
-			b=[]int{}
+		} else if len(a) > 0 {
+			tmp = append(tmp, a...)
+			a = []int{}
+		} else if len(b) > 0 {
+			tmp = append(tmp, b...)
+			b = []int{}
 		}
 	}
-	copy(list,tmp)
+	copy(list, tmp)
 }
 
-func sorted(list []int) bool {
-	if len(list)<=1 {
-		return true
+func smergesort(list []int) {
+	if len(list) == 1 {
+		return
 	}
-	last:=list[0]
-	for i:=1;i<len(list);i++ {
-		if list[i]<last {
-			return false
+	for size := 1; size < len(list); size += size {
+		for i := 0; i < len(list)-size; i += (size + size) {
+			limit := i + (2 * size)
+			if limit > len(list) {
+				limit = len(list)
+			}
+			swapOnlyMerge(list[i:limit], size)
 		}
-		last=list[i]
 	}
-	return true
 }
 
-func main() {
-	s:=mergesort([]int{9,6,5,3,1,8,7,2,4})
-	fmt.Println("Sorted list:",s)
-	fmt.Println("Sorted?",sorted(s))
+func swapOnlyMerge(list []int, size int) {
+	o := 0
+	a := size
+	for ; o < size; o++ {
+		if o == a {
+			a++
+		}
+		if list[a] < list[o] {
+			list[a], list[o] = list[o], list[a]
+		}
+		for b := a; b < len(list)-1 && list[b+1] < list[b]; b++ {
+			list[b], list[b+1] = list[b+1], list[b]
+		}
+	}
 }
+
